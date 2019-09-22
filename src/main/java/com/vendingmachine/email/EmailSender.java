3e8955fbd390c15a6ca79f81e0dca5024728cfdd
Dinu Balan <dinu.balan@null.net>
2019-09-23 00:58:19 +0300

    updated the e-mail module

--- src/main/java/com/vendingmachine/email/EmailSender.java
+++ src/main/java/com/vendingmachine/email/EmailSender.java
@@ -7,49 +7,47 @@ import java.security.Security;
 import java.util.Properties;
 
 public class EmailSender {
-	private static final String FILE_PATH = "src/main/resources/";
+    private static final String FILE_PATH = "src/main/resources/";
 
-//	public void sendEmail(String to) {
-//		Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-//		Properties props = new Properties();
-//		props.setProperty("mail.transport.protocol", "smtp");
-//		props.setProperty("mail.host", "smtp.gmail.com");
-//		props.put("mail.smtp.auth", "true");
-//		props.put("mail.smtp.port", "465");
-//		props.put("mail.smtp.socketFactory.port", "465");
-//		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-//		props.put("mail.smtp.socketFactory.fallback", "false");
-//		props.setProperty("mail.smtp.quitwait", "false");
-//		Session session = Session.getInstance(props, new Authenticator() {
-//			@Override
-//			protected PasswordAuthentication getPasswordAuthentication() {
-//				return new PasswordAuthentication("gh.iservit@gmail.com", "iservit.obr");
-//			}
-//		});
-//		try {
-//			Message message = new MimeMessage(session);
-//			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-//			message.setSubject("VendingMachine");
-//
-//			String msg = "HELLLOOOO !!!! This is an email from your awesome VendingMachine !!!";
-//
-//			MimeBodyPart mimeBodyPart = new MimeBodyPart();
-//			mimeBodyPart.setContent(msg, "text/html");
-//
-//			MimeBodyPart attachmentBodyPart = new MimeBodyPart();
-//			attachmentBodyPart.attachFile(new File(FILE_PATH + "Report1.pdf"));
-//
-//			Multipart multipart = new MimeMultipart();
-//			multipart.addBodyPart(mimeBodyPart);
-//			multipart.addBodyPart(attachmentBodyPart);
-//
-//			message.setContent(multipart);
-//
-//			Transport.send(message);
-//			System.out.println("Sending mail to ... " + to);
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
+    public void sendEmail(String to) {
+        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+        Properties props = new Properties();
+        props.setProperty("mail.transport.protocol", "smtp");
+        props.setProperty("mail.host", "smtp.gmail.com");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.quitwait", "false");
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("", "");
+            }
+        });
+        try {
+            Message message = new MimeMessage(session);
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject("VendingMachine");
+
+            String msg = "This is an email from your awesome VendingMachine !!!";
+
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(msg, "text/html");
+
+            MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+            attachmentBodyPart.attachFile(new File(FILE_PATH + "Report1.pdf"));
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+            multipart.addBodyPart(attachmentBodyPart);
+
+            message.setContent(multipart);
+
+            Transport.send(message);
+            System.out.println("Sending mail to ... " + to);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
